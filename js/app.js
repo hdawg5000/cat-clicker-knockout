1e003f816936cf7fad1722be4e573558b14849a0
Hammad Naseem <hammadnsm@gmail.com>
2017-06-27 16:05:41 -0500

    Refactor code to support multiple cats

--- js/app.js
+++ js/app.js
@@ -1,24 +1,31 @@
 var ViewModel = function() {
 
-  this.currentCat = ko.observable(new Cat());
+  this.currentCat = ko.observable(new Cat({
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/',
+    nicknames: [
+      { name: "Hammad" },
+      { name: "Hammy" },
+      { name: "hdawg" }
+    ]
+
+  }));
 
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
-    this.currentCat().level();
+    this.clickCount(this.clickCount() + 1);
+    this.level();
   };
 
 };
 
-var Cat = function() {
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('https://www.flickr.com/photos/');
-  this.nicknames = ko.observableArray([
-    { name: "Hammad" },
-    { name: "Hammy" },
-    { name: "hdawg" }
-  ]);
+var Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.level = ko.computed(function() {
     if (this.clickCount() > 10) {
