98d179cb2cb898da678afc78d9c5dbb1beff24c0
Hammad Naseem <hammadnsm@gmail.com>
2017-06-27 15:52:09 -0500

    Moved Cat observables into a separate variable

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 var ViewModel = function() {
+
+  this.currentCat = ko.observable(new Cat());
+
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    this.currentCat().level();
+  };
+
+};
+
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable('https://www.flickr.com/photos/');
-
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-    this.level();
-  };
+  this.nicknames = ko.observableArray([
+    { name: "Hammad" },
+    { name: "Hammy" },
+    { name: "hdawg" }
+  ]);
 
   this.level = ko.computed(function() {
     if (this.clickCount() > 10) {
@@ -15,13 +26,6 @@ var ViewModel = function() {
     }
     return "Newborn";
   }, this);
-
-  this.nicknames = ko.observableArray([
-    { name: "Hammad" },
-    { name: "Hammy" },
-    { name: "hdawg" }
-  ]);
-
 };
 
 ko.applyBindings(new ViewModel());
